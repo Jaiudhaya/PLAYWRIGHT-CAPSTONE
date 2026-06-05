6c0067d62b2e3c92ee6df7003dfaebc91ab0ac07
Jai Udhaya <jaiudhaya@gmail.com>
2026-06-05 13:42:57 +0530

    updates

--- Pages/dashboardPage.js
+++ Pages/dashboardPage.js
@@ -1,31 +1,16 @@
 class DashboardPage {
     constructor(page) {
+        
         this.page = page;
         this.sidebar = page.locator('.oxd-sidepanel');
-
-        this.adminMenu =
-            page.getByRole('link', { name: 'Admin' });
-
-        this.pimMenu =
-            page.getByRole('link', { name: 'PIM' });
-
-        this.leaveMenu =
-            page.getByRole('link', { name: 'Leave' });
-
-        this.timeMenu =
-            page.getByRole('link', { name: 'Time' });
-
-        this.recruitmentMenu =
-            page.getByRole('link', { name: 'Recruitment' });
-
-        this.myInfoMenu =
-            page.getByRole('link', { name: 'My Info' });
-
-        this.performanceMenu =
-            page.getByRole('link', { name: 'Performance' });
-
+        this.adminMenu = page.getByRole('link', { name: 'Admin' });
+        this.pimMenu = page.getByRole('link', { name: 'PIM' });
+        this.leaveMenu = page.getByRole('link', { name: 'Leave' });
+        this.timeMenu = page.getByRole('link', { name: 'Time' });
+        this.recruitmentMenu = page.getByRole('link', { name: 'Recruitment' });
+        this.myInfoMenu = page.getByRole('link', { name: 'My Info' });
+        this.performanceMenu = page.getByRole('link', { name: 'Performance' });
         this.searchBox = page.locator('input[placeholder="Search"]');
-
         this.directoryMenu = page.getByRole('link', { name: 'Directory' });
     }
     async isDashboardLoaded() {
